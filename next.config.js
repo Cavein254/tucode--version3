f1e98a372974c0430c1c4973a0901a8d6f2d3af1
johnmuthua <caveomuthama@gmail.com>
2022-09-04 13:50:19 +0300

    chore:ignored types

--- next.config.js
+++ next.config.js
@@ -32,4 +32,11 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
